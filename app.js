6a8246e73952ce1e4a568e6c67f31b5fdb024b5f
Lina <maggie.mengqili@gmail.com>
2020-07-19 15:53:07 -0700

    second commit: changed .env, starting port code.

--- app.js
+++ app.js
@@ -363,6 +363,10 @@ if (port == null || port == "") {
   port = 3000;
 }
 
-app.listen(port, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+// app.listen(port, function() {
+//   console.log("Server started on port 3000");
+// });
